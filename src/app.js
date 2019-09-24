26942e3522fce6da1dc3ba1720341afd5849f8fe
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-09-25 01:55:17 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const chalk = require('chalk')
+const path = require('path')
+const hbs = require('hbs')
+let my_requests = require('./requests')
+
+const app = express();
+
+
+//Setting up express and hbs
+app.set('view engine', 'hbs')
+
+const publicPath = path.join(__dirname, '../public')
+app.use(express.static(publicPath))
+
+const viewsPath = path.join(__dirname, '../templates/views')
+app.set('views', viewsPath)
+
+const partialsPath = path.join(__dirname, '../templates/partials')
+hbs.registerPartials(partialsPath)
+
+
+//routes
+
+const main_url = 'https://api.punkapi.com/v2/beers/'
+
+app.get('/', (req, res)=>{
+  res.render('index', {})
+})
+
+app.get('/random-beer', (req, res)=>{
+  const url = main_url + 'random'
+  my_requests.get_info(res, url)
+})
+
+app.get('/first-beer', (req, res)=>{
+  const url = main_url + '1'
+  my_requests.get_info(res, url)
+})
+
+app.get('*', (req, res)=>{
+  res.send('<h1>Page not found</h1>')
+})
+
+
+
+//listening to the app
+app.listen(3000, ()=>{
+  console.log(chalk.green.inverse("Listening in port 3000"))
+})
---
+++ src/requests.js
@@ -0,0 +1,20 @@
+const request = require('request')
+const chalk = require('chalk')
+
+
+const my_request = (res_int, url_int)=>{
+  const url = url_int
+  request({ url: url, json: true}, (error ,response)=>{
+    if(!error){
+      res_int.render('one_beer', {
+        name: response.body[0].name
+      })      
+    } else {
+      console.log(chalk.red('Error while trying to load the API'))
+    }
+  })
+}
+
+module.exports = {
+  get_info: my_request
+}
