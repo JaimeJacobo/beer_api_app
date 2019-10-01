6ab483dabb51570475240b9e49767d2e97c58909
Jaime Jacobo <jaimejacobordv@gmail.com>
2019-10-01 16:50:41 +0200

    starting to work on the frontend of the random beer page

--- src/requests.js
+++ src/requests.js
@@ -3,16 +3,38 @@ const chalk = require('chalk')
 
 
 const get_info = (res_int, url_int, page_int)=>{
-  let arrayOfNames = []
+  let arrayOfNames = [];
+  let arrayOfMalt = [];
+  let arrayOfHops = [];
+  let arrayOfYeast = [];
   const url = url_int
   request({ url: url, json: true}, (error ,response)=>{
-    if(!error){        
+    if(!error){ 
+
       for(let i = 0; i < response.body.length; i++){
         arrayOfNames.push(response.body[i].name)
       }
+
+      for(let i = 0; i < response.body[0].ingredients.malt.length; i++){
+        arrayOfMalt.push(response.body[0].ingredients.malt[i].name)
+      }
+
+      for(let i = 0; i < response.body[0].ingredients.hops.length; i++){
+        arrayOfHops.push(response.body[0].ingredients.hops[i].name)
+      }
+
+      arrayOfYeast.push(response.body[0].ingredients.yeast)
+
+      
       res_int.render(page_int, {
+        array: arrayOfNames,
         name: response.body[0].name,
-        array: arrayOfNames
+        tagline: response.body[0].tagline,
+        image: response.body[0].image_url,
+        malt: arrayOfMalt,
+        hops: arrayOfHops,
+        yeast: arrayOfYeast,
+        tips: response.body[0].brewers_tips
       })      
     } else {
       console.log(chalk.red('Error while trying to load the API'))
